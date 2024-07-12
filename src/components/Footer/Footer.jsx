ba11275fbe46364065499f62c2465afea82d9388
Chiranjibi Biswal <chiranjibibiswal2002@gmail.com>
2024-07-12 20:46:00 +0530

    Complete appdownload and footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Footer.css"
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className='footer-content'>
+        <div className='footer-content-left'>
+            <h2 id='logo'>Foodies.</h2>
+            <p>You can order when ever you want and whatever you want. </p>
+            <div className='footer-social-icon'>
+                <img src={assets.facebook_icon} />
+                <img src={assets.twitter_icon} />
+                <img src={assets.linkedin_icon} />
+            </div>
+        </div>
+        <div className='footer-content-center'>
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className='footer-content-right'>
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+91-78432906</li>
+                <li>contact@foodies.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>Copyright {new Date().getFullYear()} © Foodies.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
